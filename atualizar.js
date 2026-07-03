e5b90b0c564d9998c0b415a8a0c50c60b830252d
Lidertransportes <wbmnet@gmail.com>
2026-07-03 15:07:22 -0300

    Create atualizar.js

---
+++ atualizar.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const https = require("https");
+const csv = require("csv-parser");
+
+const URL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vQWv34_S3TGjQmrCeo_IK-5fOdstCk8x4o4pieq4YGkAvVDbKtOjWnsMhSsJQyxlMWLpjv0bCtR3UBN/pub?output=csv";
+
+const dados = [];
+
+https.get(URL, response => {
+
+    response
+    .pipe(csv())
+
+    .on("data", row => {
+
+        dados.push({
+
+            data: row.Data,
+            link: row.Link
+
+        });
+
+    })
+
+    .on("end", () => {
+
+        fs.writeFileSync(
+            "dados.json",
+            JSON.stringify(dados, null, 4),
+            "utf8"
+        );
+
+        console.log("dados.json atualizado.");
+
+    });
+
+});
